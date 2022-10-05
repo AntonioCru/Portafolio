a391801990f0cf3e8bc612a110b80ddf56009076
Guillermo Loza <guillermo.loza@vantis.mx>
2022-10-05 07:54:28 -0500

    [feat] Container1, Menu: Elementos se animan cuando entrar al viewport.

--- src/components/Container1.jsx
+++ src/components/Container1.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import styles from '../styles/StylesContainer1.module.css';
+import React, { useEffect, useRef } from "react";
+import styles from "../styles/StylesContainer1.module.css";
 
 export const Container1 = () => {
+  const titulo = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const target = entry.target;
+
+        if (entry.isIntersecting) {
+          target.classList.add(styles.animate__bounce);
+        }
+
+      });
+    });
+
+    console.log(titulo.current);
+    observer.observe(titulo.current);
+  }, []);
+
   return (
     <div className={styles.container1}>
-      <h1 className={styles.title}>Hola, soy Antony, desarrollador web</h1>
+      <h1 className={styles.title} ref={titulo}>
+        Hola, soy Antony, desarrollador web
+      </h1>
     </div>
-  )
-}
+  );
+};
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,33 +1,87 @@
-import React from "react";
-import styles from "../styles/StylesMenu.module.css";
-import icon from "../img/icon.png";
-import iconFacebook from '../img/facebook.png'
-import iconInstagram from '../img/instagram.png'
-import iconTwitter from '../img/twitter.png'
-import "bootstrap/dist/css/bootstrap.min.css";
-
+//react
+import React, { useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
+//styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../styles/StylesMenu.module.css";
+
+//files
+import icon from "../img/icon.png";
+import iconFacebook from "../img/facebook.png";
+import iconInstagram from "../img/instagram.png";
+import iconTwitter from "../img/twitter.png";
+
 export const Menu = () => {
+  const title = useRef(null);
+  const facebook = useRef(null);
+  const instagram = useRef(null);
+  const twitter = useRef(null);
+
+  useEffect(() => {
+    let observer;
+
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const target = entry.target;
+
+        if (entry.isIntersecting) {
+          target.classList.add(styles.animate__bounce);
+        } else {
+          target.classList.remove(styles.animate__bounce);
+        }
+      });
+    }, options);
+
+    observer.observe(title.current);
+    observer.observe(facebook.current);
+    observer.observe(instagram.current);
+    observer.observe(twitter.current);
+    return undefined;
+  }, []);
+
   return (
     <div className={styles.menuContainer}>
       <img className={styles.logo} src={icon} alt="logo" />
       <h2 className={styles.name}>Antony</h2>
-      <h5 className={styles.subTitle}>Desarrollador Web</h5>
+      <h5 className={styles.subTitle} ref={title}>
+        Desarrollador Web
+      </h5>
 
       <ButtonGroup className={styles.buttonGroup} vertical>
-        <Button variant='dark'>Sobre</Button>
-        <Button variant='dark'>Mis habilidades</Button>
+        <Button variant="dark">Sobre</Button>
+        <Button variant="dark">Mis habilidades</Button>
 
-        <Button variant='dark'>Trabajar</Button>
-        <Button variant='dark'>Contacto</Button>
-        <Button variant='dark'>Blog</Button>
+        <Button variant="dark">Trabajar</Button>
+        <Button variant="dark">Contacto</Button>
+        <Button variant="dark">Blog</Button>
       </ButtonGroup>
 
-      <img className={styles.iconFacebook} src={iconFacebook} alt="facebook.icon" />
-      <img className={styles.iconInstagram} src={iconInstagram} alt="instagram.icon" />
-      <img className={styles.iconTwitter} src={iconTwitter} alt="twitter.icon" />
+      <img
+        className={styles.iconFacebook + " " + styles.animateBase}
+        src={iconFacebook}
+        alt="facebook.icon"
+        ref={facebook}
+      />
+      <img
+        className={styles.iconInstagram}
+        src={iconInstagram}
+        alt="instagram.icon"
+        ref={instagram}
+      />
+      <img
+        className={styles.iconTwitter}
+        src={iconTwitter}
+        alt="twitter.icon"
+        ref={twitter}
+      />
     </div>
   );
 };
